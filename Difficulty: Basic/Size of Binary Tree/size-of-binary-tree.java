2dd9aec31d6fea1abb40452fffc311a79e21a314
Nigam Prasad Lenka <76672901+justynigam@users.noreply.github.com>
2025-08-19 23:56:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Size of Binary Tree/size-of-binary-tree.java
@@ -0,0 +1,29 @@
+/*
+
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public static int getSize(Node node) {
+       if(node == null){
+           return 0;
+       }
+       int left = getSize(node.left);
+       int right = getSize(node.right);
+       return (left + right) + 1;
+        
+    }
+}
