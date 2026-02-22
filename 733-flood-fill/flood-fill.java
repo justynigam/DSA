e330ceed0118af6726af585925594214ea13287a
-N-i-g-a-m- <76672901+justynigam@users.noreply.github.com>
2026-02-22 13:26:47 +0530

    Time: 2 ms (13.52%) | Memory: 47.2 MB (10.68%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,22 +1,32 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int initialcolor = image[sr][sc];
+
+        Queue<int[]> q = new LinkedList<>();
         if(initialcolor != color){
-            dfs(image , sr , sc  , initialcolor , color);
+            q.offer(new int[]{sr , sc});
+            image[sr][sc] = color;
+        }
+        int[][] dir = {{1,0} , {-1 , 0} , {0 , 1} , { 0 , -1}};
+
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            for(int k = 0 ; k < size ; k++){
+                int[] curr = q.poll();
+
+                for(int[] d : dir){
+                    int x = curr[0] + d[0];
+                    int y = curr[1] + d[1];
+
+                    if(x >= 0 && y >= 0 && x < image.length && y < image[0].length && image[x][y] == initialcolor){
+                        image[x][y] = color;
+                        q.offer(new int[]{x,y});
+                    }
+                }
+            }
         }
         return image;
-
-        
     }
-    private void dfs(int[][] image , int r , int c , int initialcolor ,  int newcolor){
-        if(r < 0 || r >= image.length || c < 0 || c >= image[0].length || image[r][c] != initialcolor){
-            return;
-        }
-        image[r][c] = newcolor;
 
-        dfs(image , r + 1 , c , initialcolor , newcolor);
-        dfs(image , r, c - 1 , initialcolor , newcolor);
-        dfs(image , r - 1 , c , initialcolor , newcolor);
-        dfs(image , r , c + 1 , initialcolor , newcolor);
-    }
 }
